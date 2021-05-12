b9597741d1702b6eb36923096e5643834ec6e0b5
Svetlaya87 <svetlaya87@gmail.com>
2021-05-12 23:56:34 +0300

    + start a new game istead bring answear

--- assets/js/code.js
+++ assets/js/code.js
@@ -3,6 +3,7 @@
    console.log(randomNumber);
    
    let currentAttempt=10;
+   let y=true;
    
 
    
@@ -30,10 +31,13 @@ function guessTheNumber() {
 
                 } else {
 
+                    y=true;
                     resultPlace.innerHTML=`Вы выиграли! Ваш вариант ${numberFromUser} равен моему числу`;
                     currentAttempt--;
                     let numberRoundsToWin = 10-currentAttempt;
                     numberOfAttempts.innerHTML=`Вы выиграли за ${numberRoundsToWin} раунда`;
+
+                    
 
                     return; /*currentAttempt=10, randomNumber = Math.floor(Math.random()*100)+1, console.log(randomNumber)*/ 
                
@@ -44,18 +48,28 @@ function guessTheNumber() {
 
             if (numberFromUser != randomNumber) {
 
+                y=true;
+
                 resultPlace.innerHTML=`Вы проиграли. Загаданное число ${randomNumber}!`;
                 numberOfAttempts.innerHTML=`Осталось 0 попыток.`;
+
+                currentAttempt--;
+
+                
 
                 return; /*currentAttempt=10, randomNumber = Math.floor(Math.random()*100)+1, console.log(randomNumber)*/ 
             
             } else {
+
+                y=true;
 
                 currentAttempt--;
                 numberRoundsToWin = 10-currentAttempt;
 
                 numberOfAttempts.innerHTML=`Вы выиграли за ${numberRoundsToWin} раунда`;
                 resultPlace.innerHTML=`Вы выиграли! Ваш вариант ${numberFromUser} равен моему числу!`;
+
+                
 
                 return; /*currentAttempt=10, randomNumber = Math.floor(Math.random()*100)+1, console.log(randomNumber)*/
             }   
@@ -64,8 +78,39 @@ function guessTheNumber() {
 
 
 
-    
+    if (y){
+
         
+
+        document.getElementById("GNBtn").addEventListener("click", function() {
+            document.getElementById("buttonResult").innerHTML = "Начать новую игру!";
+        });
+
+        
+        /*GNBtn.classList.add('disabled');*/
+        document.getElementById("GNBtn").addEventListener("click", function (NewGame2){
+
+        
+            NewGame2.location.reload();
+        });
+
+        NewGame.classList.add('d-none');
+
+       
+    } /*else {
+        /*GNBtn.classList.remove('disabled');*/
+      /*  let tag2= document.getElementById("buttonResult");
+        tag2.removeEventListener("click", function(NewGame2)
+           
+       {
+            NewGame2.location.reload();
+        } );
+    
+      /* }*/
+    
+      
+     
+    
                 
     
 }
@@ -76,9 +121,6 @@ function NewGame() {
 } 
 
 
- 
-    
-   
 
 
 
